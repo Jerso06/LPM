1e51177ae3f7ff8b39e3def11be2d78cdc7f4fed
Jerso06 <jedsonhenrique2006@gmail.com>
2023-10-31 14:42:35 -0300

    Métodos de busca de Produto concluídos

--- 10 - Outubro/0_Projeto/cliProdAPI/src/main/java/com/example/cliProdAPI/controller/ProdutoController.java
+++ 10 - Outubro/0_Projeto/cliProdAPI/src/main/java/com/example/cliProdAPI/controller/ProdutoController.java
@@ -1,12 +1,12 @@
 package com.example.cliProdAPI.controller;
 
-import com.example.cliProdAPI.model.Produto;
 import com.example.cliProdAPI.repository.ProdutoRepository;
+import com.example.cliProdAPI.model.Produto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping ("/apiProduto")
@@ -20,4 +20,42 @@ public class ProdutoController {
     {
         prodrepo.save(prd);
     }
+
+    @GetMapping ("/todos")
+    public List<Produto> buscarTodosProdutos()
+    {
+        return prodrepo.findAll();
+    }
+
+    @GetMapping ("/todos/cod/{cod}")
+    public Optional<Produto> buscarPorCod(@PathVariable("cod") int cod)
+    {
+        return prodrepo.findById(cod);
+    }
+
+    @GetMapping ("/todos/descricao/{descricao}")
+    public List<Produto> buscarPorParteDaDescricao(@PathVariable("descricao") String desc)
+    {
+        return prodrepo.findByParteDesc(desc);
+    }
+
+    @GetMapping ("/todos/prcMenorQue/{preco}")
+    public List<Produto> buscarPrecoMenor(@PathVariable("preco") double prc)
+    {
+        return prodrepo.findByPrcMenor(prc);
+    }
+
+    @GetMapping ("/todos/marca/{marca}")
+    public List<Produto> buscarPorMarca(@PathVariable("marca") String marca)
+    {
+        return prodrepo.findByMarca(marca);
+    }
+
+    @GetMapping ("/todos/marcaPreco/{marca}/{preco}")
+    public List<Produto> buscarPorMarcaEPreco(@PathVariable("marca") String marca, @PathVariable("preco") double prc)
+    {
+        return prodrepo.findByMarcaPrc(marca, prc);
+    }
+
+
 }
